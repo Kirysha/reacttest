68e40dc82266364008f58d451071f0bcdcf43e8f
Кирилл <tkirill42@mail.ru>
2019-06-19 20:04:47 +0700

    Вроде готово

--- App.js
+++ App.js
@@ -7,25 +7,19 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,Image, FlatList } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Button, Platform, StyleSheet, Text, View,Image, FlatList,TouchableWithoutFeedback } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 type Props = {};
-export default class App extends Component<Props> {
+export class HomeScreen extends Component<Props> {
 
   constructor(props){
     super(props);
-    this.state ={ isLoading: true, dataSource:null}
+    this.state ={ isLoading: true, dataSource:null, offset:0}
   }
 
   componentDidMount(){
-    fetch('https://api.vk.com/method/photos.getAll?access_token=5b46ee0b227258593ef618f6ca616f0b483a732575426cc52c6741439238c4bc4793c740929e5fa34ce57&v=5.95&owner_id=-52654926', 
+    fetch('https://api.vk.com/method/photos.getAll?access_token=5b46ee0b227258593ef618f6ca616f0b483a732575426cc52c6741439238c4bc4793c740929e5fa34ce57&v=5.95&owner_id=-52654926&count=10&offset='+this.state.offset, 
     {
       method: 'POST',
       headers: {
@@ -59,22 +53,60 @@ export default class App extends Component<Props> {
           numColumns = {2}
           renderItem={({item, index}) =>  
             //  <Text>{val}</Text>}
+            <TouchableWithoutFeedback onPress={() => this.props.navigation.navigate('OpenPhoto', {urlItem: item.sizes[3].url})}>
           <Image key={item.id} source={{
             uri: item.sizes[3].url,
             cache: 'only-if-cached',
           }}
-          style={{width: 200, height: 200}}/>}
-        />       
+          style={{width: 200, height: 200}}/>
+          </TouchableWithoutFeedback>}
+        /> 
+        <Button
+          title="Еще"
+          onPress={() => {this.setState({offset:this.state.offset+10});this.componentDidMount()}}
+        />      
       </View>
     );} else {
     return (
 
       <View style={styles.container}>
-       <Text>Пусто</Text>
+       <Text>Загрузка</Text>
       </View>);}
   }
 }
 
+export class OpenPhotoScreen extends Component<Props> {
+  
+  render(){
+    const { navigation } = this.props;
+    const items = navigation.getParam('urlItem', 'NO-ID');
+    return <View style={styles.container}>
+      <Image  source={{
+            uri: items,
+            cache: 'only-if-cached',
+          }}
+          style={{width: 400, height: 400}}/>
+      </View>
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    OpenPhoto: OpenPhotoScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends Component<Props> {
+  render() {
+    return <AppContainer />;
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
